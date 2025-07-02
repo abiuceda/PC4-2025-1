80c15773e4584ae6def7a28ccf9718aff258f4ec
abiuceda <a20230532@pucp.edu.pe>
2025-07-01 23:33:52 -0500

    Update PC4.py

--- PC4.py
+++ PC4.py
@@ -44,7 +44,7 @@ pagina_seleccionada = st.sidebar.selectbox('Selecciona una página', paginas)
 if pagina_seleccionada == 'Inicio':
 
     # La función st.markdown permite centrar y agrandar la letra del título de la web en Streamlit.
-    st.markdown("<h1 style='text-align: center;'>Bloogie for blogers :)</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'> Abi's space :)</h1>", unsafe_allow_html=True)
 
     # <h1 style='text-align: center;'>Bibi's page</h1>: Esto es una cadena de código HTML. 
     # La etiqueta <h1> se utiliza para el encabezado principal de una página web, y 
